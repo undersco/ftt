88431789dc07d9d55dd738267a5304757fe9d5f2
Math <undersco@users.noreply.github.com>
2022-06-03 23:13:16 +0200

    old

---
+++ src/components/.old/V1__Carrousel.vue
@@ -0,0 +1,95 @@
+<template>
+  <section class="carrousel">
+    <h3>Notre Sélection</h3>
+    <div class="carrousel__container">
+      <article>
+        <div class="product__image">
+          <img src="@/assets/images/product1.jpeg" alt="" />
+          <img src="@/assets/images/product2.jpeg" alt="" />
+        </div>
+        <div class="product__description">
+          <h4>T-shirt Homme</h4>
+          <span>70,00 €</span>
+        </div>
+      </article>
+      <article>
+        <div class="product__image">
+          <img src="@/assets/images/product2.jpeg" alt="" />
+          <img src="@/assets/images/product3.jpeg" alt="" />
+        </div>
+        <div class="product__description">
+          <h4>T-shirt Homme</h4>
+          <span>70,00 €</span>
+        </div>
+      </article>
+      <article>
+        <div class="product__image">
+          <img src="@/assets/images/product3.jpeg" alt="" />
+          <img src="@/assets/images/product4.jpeg" alt="" />
+        </div>
+        <div class="product__description">
+          <h4>T-shirt Homme</h4>
+          <span>70,00 €</span>
+        </div>
+      </article>
+      <article>
+        <div class="product__image">
+          <img src="@/assets/images/product5.jpeg" alt="" />
+          <img src="@/assets/images/product6.jpeg" alt="" />
+        </div>
+        <div class="product__description">
+          <h4>T-shirt Homme</h4>
+          <span>70,00 €</span>
+        </div>
+      </article>
+    </div>
+  </section>
+</template>
+<script>
+export default {
+  name: "Carrouselv1Vue",
+};
+</script>
+<style scoped>
+.carrousel {
+  padding: 5rem 2rem;
+}
+.carrousel__container {
+  display: flex;
+  gap: 1rem;
+}
+.carrousel__container article {
+  max-width: 25%;
+  overflow: hidden;
+  flex: 1;
+}
+.product__image {
+  position: relative;
+}
+.product__image img {
+  object-fit: cover;
+  position: relative;
+  object-fit: cover;
+  width: 100%;
+  height: auto;
+  transition: all ease-in-out 0.25s;
+  margin-bottom: 1rem;
+}
+.product__image img + img {
+  position: absolute;
+  inset: 0;
+  min-height: 100%;
+  opacity: 0;
+}
+.product__image:hover img {
+  opacity: 0;
+}
+.product__image:hover img + img {
+  opacity: 1;
+}
+@media (max-width: 768px) {
+  .carrousel__container article {
+    min-width: 50%;
+  }
+}
+</style>
---
+++ src/components/.old/V2__Carrousel.vue
@@ -0,0 +1,114 @@
+<template>
+  <section class="carrousel">
+    <h3>Notre Sélection</h3>
+    <div class="carrousel__container">
+      <div class="swiper">
+        <div class="swiper-wrapper">
+          <article class="swiper-slide">
+            <div class="product__image">
+              <img src="@/assets/images/product1.jpeg" alt="" />
+              <img src="@/assets/images/product2.jpeg" alt="" />
+            </div>
+            <div class="product__description">
+              <h4>T-shirt Homme</h4>
+              <span>70,00 €</span>
+            </div>
+          </article>
+          <article class="swiper-slide">
+            <div class="product__image">
+              <img src="@/assets/images/product2.jpeg" alt="" />
+              <img src="@/assets/images/product3.jpeg" alt="" />
+            </div>
+            <div class="product__description">
+              <h4>T-shirt Homme</h4>
+              <span>70,00 €</span>
+            </div>
+          </article>
+          <article class="swiper-slide">
+            <div class="product__image">
+              <img src="@/assets/images/product3.jpeg" alt="" />
+              <img src="@/assets/images/product4.jpeg" alt="" />
+            </div>
+            <div class="product__description">
+              <h4>T-shirt Homme</h4>
+              <span>70,00 €</span>
+            </div>
+          </article>
+          <article class="swiper-slide">
+            <div class="product__image">
+              <img src="@/assets/images/product5.jpeg" alt="" />
+              <img src="@/assets/images/product6.jpeg" alt="" />
+            </div>
+            <div class="product__description">
+              <h4>T-shirt Homme</h4>
+              <span>70,00 €</span>
+            </div>
+          </article>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+<script>
+import Swiper from "swiper";
+import "swiper/css";
+export default {
+  name: "Carrouselv2Vue",
+  mounted() {
+    const swiper = new Swiper(".swiper", {
+      slidesPerView: 4,
+      spaceBetween: 10,
+      breakpoints: {
+        320: {
+          slidesPerView: 2,
+          spaceBetween: 10,
+        },
+        480: {
+          slidesPerView: 2,
+          spaceBetween: 10,
+        },
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 10,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 10,
+        },
+        769: {
+          slidesPerView: 4,
+          spaceBetween: 10,
+        },
+      },
+    });
+    console.log(swiper);
+  },
+};
+</script>
+<style scoped>
+.carrousel {
+  padding: 5rem 2rem;
+}
+.product__image {
+  position: relative;
+  overflow: hidden;
+}
+.product__image > img {
+  position: relative;
+  min-height: 100%;
+  object-fit: fill;
+  transition: all ease-in 0.25s;
+  opacity: 1;
+}
+.product__image img + img {
+  position: absolute;
+  inset: 0;
+  opacity: 0;
+}
+.product__image:hover > img {
+  opacity: 0;
+}
+.product__image:hover img + img {
+  opacity: 1;
+}
+</style>
